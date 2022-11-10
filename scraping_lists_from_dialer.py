42fa84c615dd0fcccfba5906aea0369c41218da3
AbdelRahman ali <AbdelRahman.ali@pos.com>
2022-11-10 23:44:28 +0200

    changing read me

--- scraping_lists_from_dialer.py
+++ scraping_lists_from_dialer.py
@@ -53,52 +53,6 @@ used_date = year+'-'+month+'-'+ day
 
 
 
-def get_attendance(date):
-    print(date)
-    session = HTMLSession()
-    session.auth = (user, password)
-    for server in ['',2,4]:
-        time_list = {}
-        start = time.time()
-        ext = 0
-        if server== 4 :
-            ext = 9040
-        else:
-            ext = 9151
-        for num in range(9001,ext):
-
-            prog = (num-9000)/(ext-9000)
-            pause = 'r'
-            while True :
-                try:
-                    hostname = 'pos'+str(server)+'.mscall.net/vicidial/user_stats.php?DB=0&NVAuser=&did_id=&did=&pause_code_rpt=&park_rpt=&begin_date='+date+'&end_date='+date+'&user='+str(num)+'&call_status=&submit=submit'
-            
-
-
-                    auth = session.post('http://' + hostname,timeout=20)
-                    response = session.get('http://' + hostname )
-                    bs = BeautifulSoup(response.text,"html.parser")
-                    result = bs.find_all('center')[4].find_all('td')[11].text
-                    #print(num, result)
-                    time_list[num] = [result,f'pos{server}']
-                    break
-                except:
-                    print('connection lost trying again...')
-                pause = input_with_timeout('',2)
-                while pause != 'r':
-                    pause = input('the scraping process is paused press any key to continue!',2)
-            os.system('cls')
-            print(f'scraping pos{server} data ==>')
-            print('|','='*round(20*prog),' '*round(20*(1-prog)),'|',str(round((prog*100),2)),'%')
-            
-
-        #saving the results in dataframes then convert it to excel 
-        df = pd.DataFrame(index= time_list.keys(),data=time_list.values())
-        df.to_excel(f'd:/php_db/txt/scraping the new data/{date}POS{server}.xlsx')
-        end = time.time()
-        print(f'the pos{server} list took : ',round((end-start)/60),'min')
-        
-
 
 
 def get_dates_and_run(dates):
@@ -128,6 +82,55 @@ def input_with_timeout(message, timeout):
     return None
 
 
+
+def get_attendance(date):
+    print(date)
+    session = HTMLSession()
+    session.auth = (user, password)
+    for server in ['',2,4]:
+        time_list = {}
+        start = time.time()
+        ext = 0
+        if server== 4 :
+            ext = 9040
+        else:
+            ext = 9151
+        pause = 'r'
+        for num in range(9001,ext):
+
+            prog = (num-9000)/(ext-9000)
+            
+            while True :
+##                pause = input_with_timeout('if you want to pause press (p)',3)
+##                if pause == 'p':
+##                    pause = input('the scraping process is paused press any key to continue!')
+                try:
+                    hostname = 'pos'+str(server)+'.mscall.net/vicidial/user_stats.php?DB=0&NVAuser=&did_id=&did=&pause_code_rpt=&park_rpt=&begin_date='+date+'&end_date='+date+'&user='+str(num)+'&call_status=&submit=submit'
+            
+
+
+                    auth = session.post('http://' + hostname,timeout=20)
+                    response = session.get('http://' + hostname )
+                    bs = BeautifulSoup(response.text,"html.parser")
+                    result = bs.find_all('center')[4].find_all('td')[11].text
+                    #print(num, result)
+                    time_list[num] = [result,f'pos{server}']
+                    break
+                except:
+                    print('connection lost trying again...')
+                
+
+            os.system('cls')
+            print(f'scraping pos{server} data ==>')
+            print('|','='*round(20*prog),' '*round(20*(1-prog)),'|',str(round((prog*100),2)),'%')
+            
+
+        #saving the results in dataframes then convert it to excel 
+        df = pd.DataFrame(index= time_list.keys(),data=time_list.values())
+        df.to_excel(f'd:/php_db/txt/scraping the new data/{date}POS{server}.xlsx')
+        end = time.time()
+        print(f'the pos{server} list took : ',round((end-start)/60),'min')
+        
 
 choice = input_with_timeout("Commands: 1 ==> noraml 2 ==> run now:", 5)
 time.sleep(3)
