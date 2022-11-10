3718e3addd60b0492b232f93778781c57a23afad
AbdelRahman ali <AbdelRahman.ali@pos.com>
2022-11-10 23:48:07 +0200

    adding custome date

--- scraping_lists_from_dialer.py
+++ scraping_lists_from_dialer.py
@@ -29,43 +29,6 @@ user= "posds2"
 password = "5Y0v2LQyk87"
 
 
-current_date = datetime.datetime.now()
-
-if current_date.weekday() == 0:
-    current_date = current_date-datetime.timedelta(days=3)
-else:
-    current_date = current_date-datetime.timedelta(days=1)
-
-
-
-
-year = str(current_date.year)
-month = str(current_date.month) if(len(str(current_date.month))) >= 2 else '0'+str(current_date.month)
-day = str(current_date.day) if(len(str(current_date.day))) >= 2 else '0'+str(current_date.day)
-
-
-
-
-used_date = year+'-'+month+'-'+ day
-
-
-
-
-
-
-
-
-def get_dates_and_run(dates):
-    for date in dates:
-        get_attendance(date)
-
-#get the user choice
-
-def get_input(message, channel):
-    response = input(message)
-    channel.put(response)
-
-
 def input_with_timeout(message, timeout):
     channel = queue.Queue()
     message = message + " [{} sec timeout]:".format(timeout)
@@ -82,12 +45,11 @@ def input_with_timeout(message, timeout):
     return None
 
 
-
 def get_attendance(date):
     print(date)
     session = HTMLSession()
     session.auth = (user, password)
-    for server in ['',2,4]:
+    for server in ['',4,2]:
         time_list = {}
         start = time.time()
         ext = 0
@@ -95,15 +57,10 @@ def get_attendance(date):
             ext = 9040
         else:
             ext = 9151
-        pause = 'r'
         for num in range(9001,ext):
 
             prog = (num-9000)/(ext-9000)
-            
             while True :
-##                pause = input_with_timeout('if you want to pause press (p)',3)
-##                if pause == 'p':
-##                    pause = input('the scraping process is paused press any key to continue!')
                 try:
                     hostname = 'pos'+str(server)+'.mscall.net/vicidial/user_stats.php?DB=0&NVAuser=&did_id=&did=&pause_code_rpt=&park_rpt=&begin_date='+date+'&end_date='+date+'&user='+str(num)+'&call_status=&submit=submit'
             
@@ -118,8 +75,6 @@ def get_attendance(date):
                     break
                 except:
                     print('connection lost trying again...')
-                
-
             os.system('cls')
             print(f'scraping pos{server} data ==>')
             print('|','='*round(20*prog),' '*round(20*(1-prog)),'|',str(round((prog*100),2)),'%')
@@ -132,6 +87,40 @@ def get_attendance(date):
         print(f'the pos{server} list took : ',round((end-start)/60),'min')
         
 
+
+
+def get_dates_and_run(dates):
+    for date in dates:
+        get_attendance(date)
+
+#get the user choice
+
+def get_input(message, channel):
+    response = input(message)
+    channel.put(response)
+
+
+used_date = input_with_timeout('please enter the required date if you need the previous date please press enter : ',10)
+
+if used_date == '':
+    #get the needed date if the user did't enter one
+    current_date = datetime.datetime.now()
+
+    if current_date.weekday() == 0:
+        current_date = current_date-datetime.timedelta(days=3)
+    else:
+        current_date = current_date-datetime.timedelta(days=1)
+
+    year = str(current_date.year)
+    month = str(current_date.month) if(len(str(current_date.month))) >= 2 else '0'+str(current_date.month)
+    day = str(current_date.day) if(len(str(current_date.day))) >= 2 else '0'+str(current_date.day)
+
+    used_date = year+'-'+month+'-'+ day
+
+
+
+
+
 choice = input_with_timeout("Commands: 1 ==> noraml 2 ==> run now:", 5)
 time.sleep(3)
 
@@ -139,7 +128,8 @@ time.sleep(3)
 
 
 while True:
-    if ((time.localtime().tm_hour > 0 or time.localtime().tm_min > 31)and (choice != '2')):
+    if ((time.localtime().tm_hour > 1 or time.localtime().tm_min > 50) and (choice != '2')):
+        print((time.localtime().tm_hour > 1 or time.localtime().tm_min > 31) and (choice != '2'))
         os.system('cls')
         print(f'the time remaining for the next scrape is {23- time.localtime().tm_hour} h ' +
         f'{60 - time.localtime().tm_min} m {60 - time.localtime().tm_sec} s ')
@@ -150,7 +140,8 @@ while True:
         date = [used_date]
         get_dates_and_run(date)
         choice = '1'
-    
+
+# or time.localtime().tm_min > 31
 
 
 
